5bcf44ff4d564dccc33b86cfa3808744c168e101
ronger <ronger-x@outlook.com>
2020-04-02 10:34:01 +0800

    first commit

---
+++ src/main/java/com/rymcu/peanut/PeanutApplication.java
@@ -0,0 +1,13 @@
+package com.rymcu.peanut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeanutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeanutApplication.class, args);
+	}
+
+}
